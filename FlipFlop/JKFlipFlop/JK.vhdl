a0ec15f2634601a3f0c879c6548636d432bc15f8
erfan.goodboy <erfanebrahimi7878@gmail.com>
2021-04-16 15:12:17 +0430

    JK flip flop

--- FlipFlop/JKFlipFlop/JK.vhdl
+++ FlipFlop/JKFlipFlop/JK.vhdl
@@ -1,19 +1,31 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
- 
-entity FA is
- Port ( A : in STD_LOGIC;
- B : in STD_LOGIC;
- Cin : in STD_LOGIC;
- S : out STD_LOGIC;
- Cout : out STD_LOGIC);
-end FA;
- 
-architecture gate_level of FA is
- 
+library ieee;
+use ieee.std_logic_1164.all;
+entity JK is
+port(
+    J,K: in std_logic;
+    Q,NQ: buffer std_logic
+);
+end JK;
+architecture flipflop of JK is
 begin
- 
- S <= A XOR B XOR Cin ;
- Cout <= (A AND B) OR (Cin AND A) OR (Cin AND B) ;
- 
-end gate_level;
+    process (J,K)
+    begin
+    if J ='0' and K='0' then 
+        Q <= Q;
+        NQ <= NOT Q;
+        end if;
+    if J ='0' and K='1' then 
+        Q <= '0';
+        NQ <= '1';
+        end if;
+    if J ='1' and K='0' then 
+        Q <= '1';
+        NQ <= '0';
+        end if;
+    if J ='1' and K='1' then 
+        Q <= NOT Q;
+      NQ <= NOT NQ;
+        end if;
+
+    end process; 
+end flipflop;
